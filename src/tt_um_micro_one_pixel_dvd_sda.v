a7de8381edad0e19e9f5a79b555cecd15105e5de
Mike Bell <mdb036@gmail.com>
2024-09-06 18:31:30 +0100

    Try something

--- src/tt_um_micro_one_pixel_dvd_sda.v
+++ src/tt_um_micro_one_pixel_dvd_sda.v
@@ -45,23 +45,17 @@ module tt_um_micro_one_pixel_dvd_sda (
 
   assign {R, G, B} = video_active ? (((pix_x[9:5] == dvd_x) && (pix_y[8:5] == dvd_y)) ? ui_in[5:0] : ~ui_in[5:0]) : 6'b00_00_00;
   
-  reg en_vsync;
-  reg vsync2;
+  reg vsync_last;
   reg en_vsync2;
 
   always @(posedge clk) begin
     if (~rst_n) begin
-      vsync2 <= 1'b0;
-      en_vsync <= 1'b1;
+      vsync_last <= 1'b1;
+      en_vsync2 <= 1'b0;
     end else begin
-      if (en_vsync) begin
-          if (vsync) begin
-            en_vsync <= 1'b0;
-            vsync2 <= ~vsync2;
-          end
-      end else begin
-        if (~vsync)
-          en_vsync <= 1'b1;
+      vsync_last <= vsync;
+      if (!vsync_last && vsync) begin
+          en_vsync2 <= ~en_vsync2;
       end
     end
   end
@@ -72,40 +66,31 @@ module tt_um_micro_one_pixel_dvd_sda (
       dvd_y <= 4'd1;
       dir_x <= ui_in[7];
       dir_y <= ui_in[6];
-      
-      en_vsync2 <= 1'b1;
     end else begin
-      if (en_vsync2) begin
-          if (vsync2) begin
-            en_vsync2 <= 1'b0;
+      if (en_vsync2 && !vsync_last && vsync) begin
+        if (dir_x) begin
+          if (dvd_x == 5'd19)
+            dir_x <= ~dir_x;
+          else
+            dvd_x <= dvd_x + 1;
+        end else begin
+          if (dvd_x == 5'd0)
+            dir_x <= ~dir_x;
+          else
+            dvd_x <= dvd_x - 1;
+        end
 
-            if (dir_x) begin
-              if (dvd_x == 5'd19)
-                dir_x <= ~dir_x;
-              else
-                dvd_x <= dvd_x + 1;
-            end else begin
-              if (dvd_x == 5'd0)
-                dir_x <= ~dir_x;
-              else
-                dvd_x <= dvd_x - 1;
-            end
-
-            if (dir_y) begin
-              if (dvd_y == 4'd14)
-                dir_y <= ~dir_y;
-              else
-                dvd_y <= dvd_y + 1;
-            end else begin
-              if (dvd_y == 4'd0)
-                dir_y <= ~dir_y;
-              else
-                dvd_y <= dvd_y - 1;
-            end
-          end
-      end else begin
-        if (~vsync2)
-          en_vsync2 <= 1'b1;
+        if (dir_y) begin
+          if (dvd_y == 4'd14)
+            dir_y <= ~dir_y;
+          else
+            dvd_y <= dvd_y + 1;
+        end else begin
+          if (dvd_y == 4'd0)
+            dir_y <= ~dir_y;
+          else
+            dvd_y <= dvd_y - 1;
+        end
       end
     end
   end
